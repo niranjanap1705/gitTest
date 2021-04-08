548b26c6f9d715f6301a8c36a734cdb3022b4781
niranjanap1705 <niranjanap1705@gmail.com>
2021-04-08 16:20:52 -0700

    my first commit

---
+++ displayAtoZ.java
@@ -0,0 +1,22 @@
+package SamplePrograms;
+
+public class displayAtoZ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=65;i<91;i++)
+		{
+			
+			char a=(char)i;
+			System.out.println(a);
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
